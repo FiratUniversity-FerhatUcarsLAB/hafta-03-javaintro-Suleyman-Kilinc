4bb1115de8d178a4d498cbf56bba1f5cefbc259d
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 18:59:38 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,16 @@
+
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+
+        double en = 4.5;
+        double boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.println("Eni 4.5 ve boyu 7.9 olan dikdörtgen için:");
+        System.out.println("Alan: " + alan);
+        System.out.println("Çevre: " + cevre);
+
+    }
+}
