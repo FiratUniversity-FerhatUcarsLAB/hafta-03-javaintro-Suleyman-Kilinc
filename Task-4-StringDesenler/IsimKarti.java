4e96484c7b12cc378b918c4b00ef7d9192163f9e
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 19:17:52 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,22 @@
+
+public class IsimKarti {
+    public static void main(String[] args) {
+
+        String ad = "Süleyman";
+        String soyad = "Kılınç";
+        String ogrenciNo = "250541031";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("******************************************");
+        System.out.println("* *");
+        System.out.println("* ÖĞRENCİ BİLGİ KARTI            *");
+        System.out.println("* *");
+        System.out.println("******************************************");
+        System.out.println("\tAdı\t\t: " + ad);
+        System.out.println("\tSoyadı\t\t: " + soyad);
+        System.out.println("\tÖğrenci No\t: " + ogrenciNo);
+        System.out.println("\tBölüm\t\t: " + bolum);
+        System.out.println("******************************************");
+
+    }
+}
