82e290c8cad4b670690d6a57a185e4d7886a7e51
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 20:32:57 +0300

    Add files via upload

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,10 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+
+        System.out.println(" SSSS   Ü   Ü  L      EEEEE  Y   Y  M   M   AAAAA  N   N");
+        System.out.println("S       Ü   Ü  L      E       Y Y   MM MM  A     A NN  N");
+        System.out.println(" SSS    Ü   Ü  L      EEEE     Y    M M M  AAAAAAA N N N");
+        System.out.println("    S   Ü   Ü  L      E        Y    M   M  A     A N  NN");
+        System.out.println("SSSS    UUUUU  LLLLL  EEEEE    Y    M   M  A     A N   N");
+    }
+}
