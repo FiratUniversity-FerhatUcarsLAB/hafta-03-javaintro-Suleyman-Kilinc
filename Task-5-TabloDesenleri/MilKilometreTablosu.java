bddaaf75c963d46dcea64ea53292f0a8fd2d5e17
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 19:40:27 +0300

    Add files via upload

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,16 @@
+
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+
+        double MIL_TO_KM = 1.609;
+
+        System.out.println("Mil --> Kilometre");
+        System.out.println("-----------------");
+
+        System.out.println("1" + " = " + (1 * MIL_TO_KM));
+        System.out.println("5" + " = " +  (5 * MIL_TO_KM));
+        System.out.println("10" + " = " +  (10 * MIL_TO_KM));
+        System.out.println("20" + " = " +  (20 * MIL_TO_KM));
+        System.out.println("50" + " = " +  (50 * MIL_TO_KM));
+    }
+}
