1ff54cd391807ee320a8d2ab0adfa80dd25c8f65
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 18:48:35 +0300

    Add files via upload

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,12 @@
+
+public class Faktoriyel {
+    public static void main(String[] args) {
+
+        System.out.println("5 Faktöriyel hesaplanıyor...");
+        System.out.println("Ara adımlar: 5! = 5 * 4 * 3 * 2 * 1");
+
+        int sonuc = 5 * 4 * 3 * 2 * 1;
+        
+        System.out.println("Sonuç: " + sonuc);
+    }
+}
