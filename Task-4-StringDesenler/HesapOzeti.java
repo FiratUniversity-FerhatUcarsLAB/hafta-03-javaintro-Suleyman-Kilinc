2be3cb3d1d279594c88d49227770f6153df095d6
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 19:25:17 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,17 @@
+
+public class HesapOzeti {
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------");
+        System.out.println("\t\tHESAP ÖZETİ");
+        System.out.println("------------------------------------------");
+        System.out.println("Ürün\t\tMiktar\t\tFiyat");
+        System.out.println("------------------------------------------");
+
+        System.out.println("Ekmek\t\t2 Adet\t\t15.00 TL");
+        System.out.println("Süt\t\t1 Litre\t\t35.50 TL");
+        System.out.println("Yumurta\t\t10 Adet\t\t55.75 TL");
+        System.out.println("Peynir\t\t500 gr\t\t150.00 TL");
+        
+        System.out.println("------------------------------------------");
+    }
+}
