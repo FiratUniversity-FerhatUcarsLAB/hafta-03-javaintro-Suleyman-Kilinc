a613547f7eeafc60ef70f39c78ae6620ceb0c4b0
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 18:38:14 +0300

    Add files via upload

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,9 @@
+
+public class ToplamaSeri {
+    public static void main(String[] args) {
+
+        int sonuc = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
+        System.out.println("1'den 9'a kadar olan sayıların toplamı: " + sonuc);
+    }
+}
