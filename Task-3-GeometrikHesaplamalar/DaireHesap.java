070854b5722d94e244c7f0d30a23383627ea282d
Suleyman-Kilinc <slmklnc2008@gmail.com>
2025-10-17 18:55:37 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,16 @@
+
+public class DaireHesap {
+    public static void main(String[] args) {
+
+        double yariCap = 5.5;
+
+        double alan = Math.PI * yariCap * yariCap;
+
+        double cevre = 2 * Math.PI * yariCap;
+
+        System.out.println("Yarıçapı 5.5 olan daire için:");
+        System.out.println("Alan: " + alan);
+        System.out.println("Çevre: " + cevre);
+
+    }
+}
